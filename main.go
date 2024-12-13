27c8e55803da1a1131667abd40441725e5725d2b
lodrg <327733082@qq.com>
2024-12-13 18:06:31 +0800

    go quick

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var wg sync.WaitGroup
+
+	ch := make(chan int)
+	a := []int{1, 2, 3}
+	b := make([]int, 0, 4)
+
+	for i := 1; i < len(a); i++ {
+		wg.Add(1)
+		go func(x, y int) {
+			defer wg.Done()
+			ch <- x + y
+		}(a[i-1], a[i])
+
+		res := <-ch
+		b = append(b, res)
+	}
+
+	wg.Wait()
+	fmt.Println(a)
+	fmt.Println(b)
 }
